b5c7c8fde284dccc117c639ca45025aaa4ccda32
Nick Schmalenberger <nick@underdark.lan>
2014-07-11 02:30:57 -0700

    hopefully this will make entrances more comprehensible in the admin interface

--- caves/models.py
+++ caves/models.py
@@ -11,8 +11,8 @@ class cave(models.Model):
 
 class entrance(models.Model):
         name = models.CharField(max_length=80, null=True, blank=True)
-        cave_id = models.ForeignKey('cave')
+        cave = models.ForeignKey('cave')
         location = models.PointField(srid=4326)
         def __str__(self):              # __unicode__ on Python 2
-                return self.name
+                return self.cave
 
