ef93637b49d92892bc6d89da5ea1e65be9593660
Nick Schmalenberger <nick@underdark.lan>
2014-07-11 02:56:37 -0700

    some more comments about the cave models

--- caves/models.py
+++ caves/models.py
@@ -1,15 +1,17 @@
 from django.contrib.gis.db import models
 
-# Create your models here.
-
-
 class cave(models.Model):
+	# every cave needs a name
+	# alternate names must refer to some other cave
+	# but not every cave is itself an alternate name
         name = models.CharField(max_length=80)
         aka = models.ForeignKey('self', null=True, blank=True)
         def __str__(self):              # __unicode__ on Python 2
                 return self.name
 
 class entrance(models.Model):
+	# not every entrance has a name
+	# but every entrance must have a cave that has a name
         name = models.CharField(max_length=80, null=True, blank=True)
         cave = models.ForeignKey('cave')
         location = models.PointField(srid=4326)
