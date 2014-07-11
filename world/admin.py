9f09792b9abc6479cd78606119e485dcc0a55266
Nick Schmalenberger <nick@underdark.lan>
2014-07-11 01:33:08 -0700

    make these singular named objects

--- world/admin.py
+++ world/admin.py
@@ -1,6 +1,8 @@
 from django.contrib.gis import admin
-from models import cnty24k09, plsa_1997, quad24
+from models import cnty24k09, plsa_1997, quad24, caves, entrances
 
 admin.site.register(cnty24k09, admin.GeoModelAdmin)
 admin.site.register(plsa_1997, admin.GeoModelAdmin)
 admin.site.register(quad24, admin.GeoModelAdmin)
+admin.site.register(caves, admin.GeoModelAdmin)
+admin.site.register(entrances, admin.GeoModelAdmin)
