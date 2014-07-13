83fa10f8cf798c741df4f7f5f1f30ad4bcc845b6
Nick Schmalenberger <nick@underdark.lan>
2014-07-13 05:03:27 -0700

    minor changes to the caves model comments

--- caves/models.py
+++ caves/models.py
@@ -6,16 +6,17 @@ class cave(models.Model):
 	# but not every cave is itself an alternate name
         name = models.CharField(max_length=80)
         aka = models.ForeignKey('self', null=True, blank=True)
-        def __str__(self):              # __unicode__ on Python 2
+        def __str__(self):
                 return self.name
 
 class entrance(models.Model):
 	# not every entrance has a name
-	# but every entrance must have a cave that has a name
+	# but every entrance must have a cave
         name = models.CharField(max_length=80, null=True, blank=True)
         cave = models.ForeignKey('cave')
         location = models.PointField(srid=4326)
-        def __str__(self):              # __unicode__ on Python 2
+        def __str__(self):
+		# if the entrance doesn't have a name, use the cave name
 		if not self.name:
 	                return self.cave.name
 		else:
