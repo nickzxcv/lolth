5cc50a406753c23dc1b509396c96cb69f0551b64
Nick Schmalenberger <nick@underdark.lan>
2014-07-13 08:09:47 -0700

    function about where locations are

--- caves/urls.py
+++ caves/urls.py
@@ -4,6 +4,6 @@ from caves import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
-	url(r'^(?P<cave_id>\d+)/name$', views.name, name='name')
+	url(r'^(?P<cave_id>\d+)/name$', views.name, name='name'),
 	url(r'^(?P<cave_id>\d+)/entrances$', views.entrances, name='entrances')
 )
--- caves/views.py
+++ caves/views.py
@@ -19,3 +19,15 @@ def name(request, cave_id):
 			response+=" {}".format(akacave.name)
 
 	return HttpResponse(response)
+
+def entrances(request, cave_id):
+	mycave=cave.objects.get(pk=cave_id)
+	entrancelist=entrance.objects.filter(cave=mycave.id)
+
+	for caveentrance in entrancelist:
+		county=cnty24k09.objects.get(geom__contains=caveentrance.location)
+		section=plsa_1997.objects.get(geom__contains=caveentrance.location)
+		quad=quad24.objects.get(geom__contains=caveentrance.location)
+		response+=" {} entrance is in {} County, {} quad, township {} range {} section {}".format(caveentrance, county, quad, section.township, section.range, section.section )
+
+	return HttpResponse(response)
